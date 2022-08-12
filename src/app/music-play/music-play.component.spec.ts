313807d62e6c294e9be4cac4feb2d597b25b4ebc
Christophe Migliorini <cmigliorini@users.noreply.github.com>
2022-08-12 09:48:51 +0200

    upgrade to angular 11.2

--- src/app/music-play/music-play.component.spec.ts
+++ src/app/music-play/music-play.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { MusicPlayComponent } from './music-play.component';
 import { Input } from '@angular/core';
@@ -9,7 +9,7 @@ describe('MusicPlayComponent', () => {
   let component: MusicPlayComponent;
   let fixture: ComponentFixture<MusicPlayComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ MusicPlayComponent ]
     })
